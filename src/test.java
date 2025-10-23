485f710cf41f264ab7615c03664caee4feaf78d1
Walidmight <benhallouwalid@gmail.com>
2025-10-23 16:54:41 -0400

    changement walid

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("on test notre fichier");
-        System.out.println("cest wallid");
+        System.out.println("cest walid");
 
     }
 }
