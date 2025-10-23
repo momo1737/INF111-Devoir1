0de20bd08a7e8ed5c46b83d9a345f5fb0de41fc5
raytosh <abbachar05@gmail.com>
2025-10-23 16:51:34 -0400

    test r

--- src/test.java
+++ src/test.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
         System.out.println("on test notre fichier");
+        Scanner a = new Scanner(System.in);
+        System.out.println("je m'appelle comment ?(dis daddy");
+        String c = a.nextLine();
+        System.out.println("tu t'appelle "+c);
     }
 }
